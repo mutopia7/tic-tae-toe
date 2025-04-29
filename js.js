3994c569fba0f381403c39d72f5b6406a5d6e8a3
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-04-29 16:49:14 +0330

    feat(player): add player factory function with basic move logic

--- js.js
+++ js.js
@@ -9,3 +9,26 @@ function printBoard(){
     board.forEach(row => console.log(row.join(" | ")))
 }
 
+function createPlayer(name , symbol){
+    return{
+        name,
+        symbol,
+        makeMove(row , col){
+            if(board[row][col] !== ""){
+                console.log("This cell completed before!")
+                return false
+            }
+
+            board[row][col] = symbol;
+            printBoard();
+            return true;
+        }
+    }
+}
+
+const player1 = createPlayer("mohamad", "X");
+const player2 = createPlayer("hanah", "O");
+
+player1.makeMove(0, 0);
+player2.makeMove(0, 0); 
+player2.makeMove(1, 1);
